37e1c13d88058957663afe6ae38e2fdfac02a43b
ye3072 <48782880+ye3072@users.noreply.github.com>
2019-07-17 10:59:41 +0900

    Update ChatText.vue

--- components/ChatText.vue
+++ components/ChatText.vue
@@ -6,7 +6,7 @@
             <div class="input-group-prepend">
                 <div class="input-group-text" style="color:rgb(125,125,125);"><Icon name="plus"/></div>
             </div>
-            <b-input id="inline-form-input-username" style="border-right:0px;" v-bind:placeholder="placehd"></b-input>
+            <b-input id="inline-form-input-username" style="border-right:0px;" v-bind:placeholder="placeholder"></b-input>
             <div class="input-group-append">
                 <div class="input-group-text emo"><Icon name="smile"/></div>
             </div>
@@ -17,17 +17,7 @@
 
 <script>
 export default {
-    props: ['placeholder'],
-    data: function() {
-        return {
-            /* 
-            
-            이게 왜 동기화가 안되는 지 모르겠음!! 
-            
-            */
-            placehd: this.$props.placeholder
-        }
-    }
+    props: ['placeholder']
 }
 </script>
 
